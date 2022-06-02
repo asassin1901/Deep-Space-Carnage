768a22a99319932759ea5ffe706f622a7a1ea1aa
asassin1901 <57683620+asassin1901@users.noreply.github.com>
2022-06-02 13:35:43 +0200

    checkpoint

---
+++ Assets/BossFightManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFightManager : MonoBehaviour
+{
+    public BoxCollider2D trigger;
+    public GameObject boss;
+    public GameObject baricade;
+    bool test = false;
+    Movement script;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player"))
+        {
+            test = true;
+            trigger.enabled = false;
+            boss.SetActive(true);
+            baricade.SetActive(true);
+
+            script = other.GetComponent<Movement>();
+            other.GetComponent<Movement>().cam.orthographicSize += 7 * Time.deltaTime;
+        }
+    }
+    private void Update() {
+        if (test)
+        {
+            script.cam.orthographicSize += 7 * Time.deltaTime;
+        }
+        if (script.cam.orthographicSize >= 10)
+        {
+            test = false;
+            script.cam.orthographicSize = 10;
+        }
+    }
+
+}
--- Assets/Scripts/AlertManager.cs
+++ Assets/Scripts/AlertManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class AlertManager : MonoBehaviour
 {
     public GameObject[] relevantEnemies;
+    private AudioManager audioManager;
+    private void Awake() {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
 
     private void Start()
     {
@@ -25,5 +29,6 @@ public class AlertManager : MonoBehaviour
             }
             GetComponent<BoxCollider2D>().enabled = false;
         }
+        audioManager.sounds[0].volume += 0.1f;
     }
 }
