18b001c3ea2fd4c1eed5f881561e5a850f7e6219
asassin1901 <57683620+asassin1901@users.noreply.github.com>
2022-06-03 01:12:02 +0200

    fixed meme

--- Assets/Scripts/volumegoessomewhere.cs
+++ Assets/Scripts/volumegoessomewhere.cs
@@ -9,21 +9,29 @@ public class volumegoessomewhere : MonoBehaviour
     public AudioMixer audioMixer;
     public Dropdown resolutionDropdown;
     Resolution[] resolutions;
-
     void Start ()
     {
-        audioMixer.SetFloat("volume", -12.7f);
         resolutions = Screen.resolutions;
+        audioMixer.SetFloat("volume", -12.7f);
 
         resolutionDropdown.ClearOptions();
 
+        int currentResolutionIndex = 0;
         List<string> options = new List<string>();
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
+            string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
         resolutionDropdown.AddOptions(options); 
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     public void SetResolution(int resolutionIndex){
