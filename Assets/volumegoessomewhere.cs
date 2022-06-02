26aca10245b80d2f75119232e9c08ee462365a57
wodniak98 <57682860+wodniak98@users.noreply.github.com>
2022-06-02 13:32:42 +0200

    audio mixer
    audio mixer

---
+++ Assets/volumegoessomewhere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class volumegoessomewhere : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume (float volume)
+    {
+
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen (bool isFullScreen)
+    {
+        isFullScreen.fullScreen = isFullScreen;
+    }
+}
