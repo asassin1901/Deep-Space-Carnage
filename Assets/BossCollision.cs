7a5fb5b973f1c842997abbc366e602c7a992f5ec
asassin1901 <57683620+asassin1901@users.noreply.github.com>
2022-06-03 03:00:15 +0200

    moreFixes

---
+++ Assets/BossCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCollision : MonoBehaviour
+{
+    int damage = 2;
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            other.gameObject.GetComponent<Movement>().Damage(damage);
+        }
+    }
+}
